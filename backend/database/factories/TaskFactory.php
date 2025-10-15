3ab9d02a15901e4827be6b780d256d4b09cf75e4
SaraAndrejic <saraandrejic2468@gmail.com>
2025-10-15 17:00:56 +0200

    Ažuriran seeder za zadatke sa novijim opsegom datuma za rokove

--- backend/database/factories/TaskFactory.php
+++ backend/database/factories/TaskFactory.php
@@ -23,7 +23,7 @@ class TaskFactory extends Factory
         'description' => $this->faker->paragraph(),
         'priority' => $this->faker->randomElement(['nizak', 'srednji', 'visok','hitno']),
         'status' => $this->faker->randomElement(['započet', 'u toku', 'završen']),
-        'deadline' => $this->faker->optional()->date(),
+        'deadline' => $this->faker->dateTimeBetween('-1 years', '+1 month'),
         'estimated_hours' => $this->faker->numberBetween(1, 20), // <--- NOVO
 
         ];
