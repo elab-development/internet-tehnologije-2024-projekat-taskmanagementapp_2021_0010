c416c3b499305c820cbf4f2a450d2e5f00863aa8
SaraAndrejic <saraandrejic2468@gmail.com>
2025-10-18 13:06:50 +0200

    Dodata login strana

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      await api.get("http://127.0.0.1:8000/sanctum/csrf-cookie", {
+        withCredentials: true,
+      });
+
+      await api.post("/login", { email, password });
+
+      // Ako uspe — preusmeri na Dashboard
+      navigate("/");
+    } catch (err) {
+      setError("Invalid credentials or server error.");
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-box">
+        <img src={logo} alt="Logo" className="app-logo" />
+        <h1 className="app-title">Task Management App</h1>
+        <p className="welcome-text">Welcome back! Please log in</p>
+
+        <form onSubmit={handleLogin} className="login-form">
+          <div className="input-group">
+            <input
+              type="email"
+              placeholder="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="pink-btn login-btn">
+            Login
+          </button>
+          <p className="auth-footer">
+        Don’t have an account? <a href="/register">Register</a>
+       </p>
+
+          {error && <p className="error">{error}</p>}
+        </form>
+      </div>
+    </div>
+  );
+}
